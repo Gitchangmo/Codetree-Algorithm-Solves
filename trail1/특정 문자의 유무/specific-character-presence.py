9ae368ad925355649972a1660ff1b22f1cc9258b
김창모 KIM CHANGMO <333rlackdah@gmail.com>
2026-06-07 22:10:22 +0900

    [Passed] 특정 문자의 유무 (54ms, 16MB)

---
+++ trail1/특정 문자의 유무/specific-character-presence.py
@@ -0,0 +1,10 @@
+string = input()
+
+if 'ee' in string and 'ab' in string:
+    print('Yes Yes')
+elif 'ee' in string:
+    print('Yes No')
+elif 'ab' in string:
+    print('No Yes')
+else:
+    print('No No')
