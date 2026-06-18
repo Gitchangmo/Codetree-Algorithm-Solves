5827a5548da6f1c85ec1076110b2f20c8ad23ff7
김창모 KIM CHANGMO <333rlackdah@gmail.com>
2026-06-18 18:18:43 +0900

    [Passed] 그 계절, 그 날 (48ms, 16MB)

--- trail2/그 계절, 그 날/that-season-that-day.py
+++ trail2/그 계절, 그 날/that-season-that-day.py
@@ -1,56 +1,33 @@
 Y, M, D = map(int, input().split())
 
 # Please write your code here.
-def year(Y):
-    if Y % 4 == 0:
-        if Y % 100 == 0:
-            if Y % 400 == 0:
-                return True
-            return False
+def is_leap_year(Y):
+    if Y % 4 != 0:
+        return False
+    if Y % 100 != 0:
+        return True
+    if Y % 400 == 0:
         return True
     return False
 
-def leaf(M, D):
+def is_valid_date(Y, M, D):
     if M == 2:
-        if D > 29:
-            return -1
-    if M == 4 or M == 6 or M == 9 or M == 11:
-        if D > 30:
-            return -1
+        max_day = 29 if is_leap_year(Y) else 28
+    elif M == 4 or M == 6 or M == 9 or M == 11:
+        max_day = 30
     else:
-        if D > 31:
-            return -1
+        max_day = 31
+    
+    return D <= max_day
 
-    if M >= 3 and M <= 5:
-        return 'Spring'
-    elif M >= 6 and M <= 8:
-        return 'Summer'
-    elif M >= 9 and M <= 11:
-        return 'Fall'
-    else:
-        return 'Winter'
-
-def common(M, D):
-    if M == 2:
-        if D > 28:
-            return -1
-    if M == 4 or M == 6 or M == 9 or M == 11:
-        if D > 30:
-            return -1
-    else:
-        if D > 31:
-            return -1
-
-    if M >= 3 and M <= 5:
-        return 'Spring'
-    elif M >= 6 and M <= 8:
-        return 'Summer'
-    elif M >= 9 and M <= 11:
-        return 'Fall'
-    else:
-        return 'Winter'
-
-if year(Y):
-    print(leaf(M, D))
+if not is_valid_date(Y, M, D):
+    print(-1)
 else:
-    print(common(M, D))
+    if 3 <= M <= 5:
+        print("Spring")
+    elif 6 <= M <= 8:
+        print("Summer")
+    elif 9 <= M <= 11:
+        print("Fall")
+    else:
+        print("Winter")
