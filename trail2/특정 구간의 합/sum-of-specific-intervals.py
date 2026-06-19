01fd992a463647ae567c18439652188ce21b2b4d
김창모 KIM CHANGMO <333rlackdah@gmail.com>
2026-06-19 16:35:14 +0900

    [Passed] 특정 구간의 합 (57ms, 17MB)

---
+++ trail2/특정 구간의 합/sum-of-specific-intervals.py
@@ -0,0 +1,8 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+queries = [tuple(map(int, input().split())) for _ in range(m)]
+
+# Please write your code here.
+for q in queries:
+    a, b = q
+    print(sum(arr[a-1:b]))
